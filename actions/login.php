9e7ec9337e70477bbb04114ccc10e3f2093c4983
Natan Costa <natanscosta7@gmail.com>
2025-10-21 23:44:51 -0300

    return null if user wasnt found

--- actions/login.php
+++ actions/login.php
@@ -2,22 +2,18 @@
 
 session_start();
 
-require_once __DIR__ . '/userActions.php';
 require_once '../includes/functions.php';
+require_once __DIR__ . '/userActions.php';
 
-if (!isset($_SESSION['user_id']) || is_null($_SESSION['user_id'])) {
-    redirect('../views/login.php');
-}
-
-$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_EMAIL);
 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_EMAIL);
 
-if (!userExists($email)) {
+$user = getOneUser($email);
+var_dump($user);
+if (is_null($user)) {
     $_SESSION['error'] = "User não encontrado";
     redirect('../views/login.php');
 }
-
-$user = getOneUser($email);
 
 if (password_verify($password, $user->password)) {
     $_SESSION['user_id'] = $user->user_id;
--- actions/userActions.php
+++ actions/userActions.php
@@ -14,22 +14,15 @@ function createUser(string $email, string $password): void
     $conn = null;
 }
 
-function userExists(string $email): bool
-{
-    $conn = connection();
-    $stmt = $conn->prepare('SELECT user_id FROM users WHERE email=:email');
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-    $conn = null;
-    return boolval($stmt->fetch());
-}
-
-function getOneUser(string $email): object
+function getOneUser(string $email)
 {
     $conn = connection();
     $stmt = $conn->prepare('SELECT user_id, password FROM users WHERE email=:email;');
     $stmt->bindParam(':email', $email);
     $stmt->execute();
     $conn = null;
+    if (!$stmt->fetch(PDO::FETCH_OBJ)) {
+        return null;
+    }
     return $stmt->fetch(PDO::FETCH_OBJ);
 }
