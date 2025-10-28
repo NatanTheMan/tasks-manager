2d49e4b2a2123819d93d13091f129c1833419535
Natan <natanscosta7@gmail.com>
2025-10-28 00:03:46 -0300

    send array with color options using controller

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -8,10 +8,15 @@ class Home
 {
     public function index()
     {
+        $colorSettings = ["low" => "#bbb", "medium" => "#d2a", "high" => "#f00"];
         $tasks = findAll(Tables::Tasks, condition:"WHERE user_id=1");
         return [
           "view" => "home.php",
-          "data" => ["title" => "Home","tasks" => $tasks]
+          "data" => [
+              "title" => "Home",
+              "tasks" => $tasks,
+              "colorSettings" => $colorSettings
+          ]
         ];
     }
 }
--- app/views/home.php
+++ app/views/home.php
@@ -1,49 +1,43 @@
-<?php
+<h1>Tarefas</h1> 
+<table> 
+<tr> 
+  <th></th>
+  <th>Tarefa</th>
+  <th>Estado</th>
+  <th>Urgência</th>
+</tr>
+<?php foreach ($tasks as $task) : ?>
+  <tr>
+    <?php if ($task->done) : ?>
+    <td>
+      <form action='../actions/undone.php?id=<?= $task->id ?>' method='post'>
+        <button type='submit'>❌</button>
+      </form>
+    </td>
+    <?php else : ?>
+    <td>
+      <form action='../actions/done.php?id=<?= $task->id ?>' method='post'>
+        <button type='submit'>✅</button>
+      </form>
+    </td>
+    <?php endif ?>
+    <td><?= $task->description ?></td>
+    <td><?= $task->done ? "concluída" : "pendente" ?></td>
 
-$colorSettings = ["low" => "#bbb", "medium" => "#d2a", "high" => "#f00"];
+    <td style="color: <?= $colorSettings[$task->urgency] ?>;"><?= $task->urgency ?></td>
 
-?>
+    <td>
+      <form action='task/<?= $task->id ?>/edit' method='get'>
+        <button type='submit'>✏️</button>
+      </form>
+    </td>
+    <td>
+      <form action='../actions/delete.php?id=<?= $task->id?>' method='post'>
+        <button type='submit'>🗑️</button>
+      </form>
+    </td>
+  </tr>
+<?php endforeach; ?>
+</table>
 
-  <h1>Tarefas</h1> 
-  <table> 
-    <tr> 
-      <th></th>
-      <th>Tarefa</th>
-      <th>Estado</th>
-      <th>Urgência</th>
-    </tr>
-    <?php foreach ($tasks as $task) : ?>
-      <tr>
-        <?php if ($task->done) : ?>
-        <td>
-          <form action='../actions/undone.php?id=<?= $task->id ?>' method='post'>
-            <button type='submit'>❌</button>
-          </form>
-        </td>
-        <?php else : ?>
-        <td>
-          <form action='../actions/done.php?id=<?= $task->id ?>' method='post'>
-            <button type='submit'>✅</button>
-          </form>
-        </td>
-        <?php endif ?>
-        <td><?= $task->description ?></td>
-        <td><?= $task->done ? "concluída" : "pendente" ?></td>
-
-        <td style="color: <?= $colorSettings[$task->urgency] ?>;"><?= $task->urgency ?></td>
-
-        <td>
-          <form action='task/<?= $task->id ?>/edit' method='get'>
-            <button type='submit'>✏️</button>
-          </form>
-        </td>
-        <td>
-          <form action='../actions/delete.php?id=<?= $task->id?>' method='post'>
-            <button type='submit'>🗑️</button>
-          </form>
-        </td>
-      </tr>
-    <?php endforeach; ?>
-  </table>
-
-   <button><a  href="../views/form_create.php">+</a></button> 
+<button><a  href="../views/form_create.php">+</a></button> 
