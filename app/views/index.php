f21a8dfdea32a26f3f9b8c11fc85c48942bb78ae
Natan <natanscosta7@gmail.com>
2025-10-28 01:10:48 -0300

    fix header links

--- app/views/index.php
+++ app/views/index.php
@@ -10,12 +10,12 @@
     <h3 style="margin-left: 40px;"><a href="/">Gerenciador de Tarefas</a></h3> 
 
     <nav style="margin-right: 60px;">
-      <a href="../views/form_create.php">Adicionar</a> 
-      <a href="../actions/logout.php">Sair ➡️</a> 
+      <a href="/task/create">Adicionar</a>
+      <a href="/logout">Sair ➡️</a>
     </nav>
   </header>
 
-  <?php require $view; ?>
+  <?php require VIEWS . $view; ?>
 
  </body>
 </html>
